57abf9913139e721338fc8df5ff66743fced6045
Kouzin1 <samuelakinwunmi2@gmail.com>
2025-02-06 12:08:13 +0000

    message

--- src/App.js
+++ src/App.js
@@ -8,24 +8,26 @@ class App extends Component {
     super();
 
     this.state = {
-      monster1: {
-        name: "Linda",
-      },
-      monster2: {
-        name: "Frank",
-      },
-      monster3: {
-        name: "Jacky",
-      },
+      monster: [
+        {
+          name: "Linda",
+        },
+        {
+          name: "Frank",
+        },
+        {
+          name: "Jacky",
+        },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <h1>{this.state.monster1.name}</h1>
-        <h1>{this.state.monster2.name}</h1>
-        <h1>{this.state.monster3.name}</h1>
+        {this.state.monster.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
