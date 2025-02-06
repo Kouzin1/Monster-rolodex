de80918f56f283c4f07dcae298054a270964f119
Kouzin1 <samuelakinwunmi2@gmail.com>
2025-02-06 11:39:17 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -4,22 +4,37 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: {
+        firstName: "Samuel",
+        lastName: "Akinwunmi",
+      },
+      company: "Wizebox",
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
+            {this.state.company}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <button
+            onClick={() => {
+              this.setState(() => {
+                return {
+                  name: { firstName: "Bobola", lastName: "Akintunde" },
+                };
+              });
+            }}
           >
-            Learn React
-          </a>
+            Change Name
+          </button>
         </header>
       </div>
     );
